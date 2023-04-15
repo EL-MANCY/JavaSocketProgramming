429b9255f7b96a8a9520a6bb0c9b717b012b42a9
EL-MANCY <abdelrahmanelmancy@gmail.com>
2023-04-15 18:12:36 +0200

    UDP New Features

--- src/UDP/CLIENT.java
+++ src/UDP/CLIENT.java
@@ -7,32 +7,37 @@ import java.util.Scanner;
 public class CLIENT {
     public static void main(String[] args) throws IOException {
 
+        boolean x=true;
         DatagramSocket datagramSocket = new DatagramSocket();
 
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
+        while (x) {
+            System.out.println("______________________________________________________________");
+            System.out.println("Enter Your Message");
+            Scanner scanner = new Scanner(System.in);
+            String input = scanner.nextLine();
 
-        byte[] outBuffer = input.getBytes();
-        InetAddress host = InetAddress.getByName("localhost");
-        int port = 12345;
+            byte[] outBuffer = input.getBytes();
+            InetAddress host = InetAddress.getByName("localhost");
+            int port = 12345;
 
-        DatagramPacket requestDatagramPacket = new DatagramPacket(outBuffer,outBuffer.length,host,port);
-        datagramSocket.send(requestDatagramPacket);
+            DatagramPacket requestDatagramPacket = new DatagramPacket(outBuffer, outBuffer.length, host, port);
+            datagramSocket.send(requestDatagramPacket);
 
-        byte[] inBuffer = new byte[1000];
-        DatagramPacket replyDatagramPacket = new DatagramPacket(inBuffer,inBuffer.length);
-        datagramSocket.receive(replyDatagramPacket);
+            byte[] inBuffer = new byte[1000];
+            DatagramPacket replyDatagramPacket = new DatagramPacket(inBuffer, inBuffer.length);
+            datagramSocket.receive(replyDatagramPacket);
 
-        String inputLine = new String(replyDatagramPacket.getData(), 0, replyDatagramPacket.getLength());
-
-
-        System.out.println("Reply: " + inputLine);
-
-
+            String inputLine = new String(replyDatagramPacket.getData(), 0, replyDatagramPacket.getLength());
 
 
+            System.out.println("Reply: " + inputLine);
 
+            if(inputLine.equals("Exiting Now")){
+                datagramSocket.close();
+                x=false;
+            }
 
+        }
 
 
 
--- src/UDP/SERVER.java
+++ src/UDP/SERVER.java
@@ -10,26 +10,33 @@ public class SERVER {
     public static void main(String[] args) throws IOException {
         DatagramSocket datagramSocket = new DatagramSocket(12345);
 
+        boolean server = true;
         byte[] inBuffer = new byte[1000];
         byte[] outBuffer = new byte[1000];
 
-        while (true){
-            DatagramPacket replyDatagramPacket = new DatagramPacket(inBuffer,inBuffer.length);
+        while (server) {
+            DatagramPacket replyDatagramPacket = new DatagramPacket(inBuffer, inBuffer.length);
             datagramSocket.receive(replyDatagramPacket);
-
             String inputLine = new String(replyDatagramPacket.getData(), 0, replyDatagramPacket.getLength());
 
-            if(inputLine.equals("ping")){
-                System.out.println(inputLine);
-                outBuffer = "pong".getBytes();
+            if (inputLine.equals("Ping")) {
+                System.out.println("CLIENT SENT: "+inputLine);
+                outBuffer = "Pong".getBytes();
                 DatagramPacket requestDatagramPacket =
-                        new DatagramPacket(outBuffer,outBuffer.length,replyDatagramPacket.getAddress(),replyDatagramPacket.getPort());
+                        new DatagramPacket(outBuffer, outBuffer.length, replyDatagramPacket.getAddress(), replyDatagramPacket.getPort());
                 datagramSocket.send(requestDatagramPacket);
-            }else{
-                outBuffer = "Wrong Answer".getBytes();
-
+            } else if (inputLine.equals("Exit")) {
+                System.out.println("CLIENT SENT: "+inputLine);
+                outBuffer = "Exiting Now".getBytes();
+                DatagramPacket ExitDatagramPacket =
+                        new DatagramPacket(outBuffer, outBuffer.length, replyDatagramPacket.getAddress(), replyDatagramPacket.getPort());
+                datagramSocket.send(ExitDatagramPacket);
+                server = false;
+            } else {
+                System.out.println("CLIENT SENT: "+inputLine);
+                outBuffer = "Not the right message".getBytes();
                 DatagramPacket requestDatagramPacket =
-                        new DatagramPacket(outBuffer,outBuffer.length,replyDatagramPacket.getAddress(),replyDatagramPacket.getPort());
+                        new DatagramPacket(outBuffer, outBuffer.length, replyDatagramPacket.getAddress(), replyDatagramPacket.getPort());
                 datagramSocket.send(requestDatagramPacket);
 
             }
