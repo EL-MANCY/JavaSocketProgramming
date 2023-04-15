88dd598973f86def0d9bf1d2cb3c30da1936f37f
EL-MANCY <abdelrahmanelmancy@gmail.com>
2023-04-15 18:12:19 +0200

    TCP New Features

--- src/TCP/Client.java
+++ src/TCP/Client.java
@@ -9,24 +9,31 @@ import java.util.Scanner;
 public class Client {
     public static void main(String[] args) throws IOException {
 
-        Scanner scanner =new Scanner(System.in);
-
+        boolean x = true;
+        Scanner scanner = new Scanner(System.in);
         Socket socket = new Socket("localhost", 1234);
-
         DataInputStream in = new DataInputStream(socket.getInputStream());
         DataOutputStream out = new DataOutputStream(socket.getOutputStream());
 
-        System.out.println("Enter Your Message");
-        String output=scanner.nextLine();
 
-        out.writeUTF(output);
+        while (x) {
+            System.out.println("______________________________________________________________");
+            System.out.println("Enter Your Message");
+            String output = scanner.nextLine();
 
-        String respone=in.readUTF();
+            out.writeUTF(output);
 
-        System.out.println(respone);
+            String resposne = in.readUTF();
+            System.out.println("SERVER RESPONDED BY: "+resposne);
 
-        in.close();
-        out.close();
-        socket.close();
+            if (resposne.equals("Connection Closed")) {
+                in.close();
+                out.close();
+                socket.close();
+                x = false;
+            }
+        }
+
+
     }
 }
--- src/TCP/Server.java
+++ src/TCP/Server.java
@@ -7,32 +7,31 @@ import java.net.Socket;
 public class Server {
     public static void main(String[] args) throws IOException {
 
-        ServerSocket serverSocket = new ServerSocket(1234);
+        try {
+            ServerSocket serverSocket = new ServerSocket(1234);
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                DataOutputStream out = new DataOutputStream(clientSocket.getOutputStream());
+                DataInputStream in = new DataInputStream(clientSocket.getInputStream());
 
-       while(true) {
-
-           Socket clientSocket = serverSocket.accept();
-
-           DataOutputStream out = new DataOutputStream(clientSocket.getOutputStream());
-           DataInputStream in = new DataInputStream(clientSocket.getInputStream());
-
-           String InputLine;
-           while ((InputLine = in.readUTF()) != null) {
-               if (InputLine.equals("ping")) {
-                   System.out.println(InputLine);
-                   out.writeUTF("pong");
-                   break;
-               } else {
-                   out.writeUTF("Wrong Answer...EXITING NOW!!");
-                   break;
-               }
-
-           }
-       }
-
-
-
+                String InputLine;
+                while ((InputLine = in.readUTF()) != null) {
+                    if (InputLine.equals("Ping")) {
+                        System.out.println("CLIENT ENTERED: " + InputLine);
+                        out.writeUTF("Pong");
+                    } else if (InputLine.equals("Exit")) {
+                        System.out.println("CLIENT ENTERED: " + InputLine);
+                        out.writeUTF("Connection Closed");
+                        clientSocket.close();
+                        break;
+                    } else {
+                        out.writeUTF("Not the right message");
+                    }
+                }
+            }
+        } catch (EOFException e) {
+            // handle EOFException
+            System.err.println("Client closed the connection unexpectedly.");
+        }
     }
-
-
 }
